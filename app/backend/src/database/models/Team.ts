3f8a45812788492c5c8bf23b48ff1abbc1923ee8
davi <davijluna@gmail.com>
2022-09-30 14:19:54 -0300

    feat: implementando models de team

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,29 @@
+import { DataTypes, Model, INTEGER } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  public id: number;
+  public teamName: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
