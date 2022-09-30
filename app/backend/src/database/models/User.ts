cbf10950c0a66beedab25448bb56a0c226a150d4
davi <davijluna@gmail.com>
2022-09-30 14:26:18 -0300

    refactor: modificacao nos arquivos

--- app/backend/src/database/models/User.ts
+++ app/backend/src/database/models/User.ts
@@ -1,41 +1,41 @@
-import { DataTypes, Model, INTEGER  } from 'sequelize';
+import { DataTypes, Model, INTEGER } from 'sequelize';
 import db from '.';
 
 class User extends Model {
-    public id: number; 
-    public username: string;  
-    public role: string;
-    public email: string;
-    public password: string;
-  }
+  public id: number;
+  public username: string;
+  public role: string;
+  public email: string;
+  public password: string;
+}
 
-  User.init({
-    id: {
-      type: INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-      role: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      }
-    // ... Campos // 
-  }, {
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  // ... Campos //
+}, {
 
-    underscored: true,
+  underscored: true,
   sequelize: db,
   // modelName: 'example',
   timestamps: false,
