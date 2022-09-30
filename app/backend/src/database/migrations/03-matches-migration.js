a246719bc0e383f6b7335c3aadfb499a1c9128ef
davi <davijluna@gmail.com>
2022-09-30 19:51:34 -0300

    feat: migration para a tabela de matches

---
+++ app/backend/src/database/migrations/03-matches-migration.js
@@ -0,0 +1,35 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('matches', {
+            id: {
+                type: Sequelize.INTEGER,
+                // allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            home_team: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            home_team_goals: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            away_team: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            away_team_goals: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            in_progress: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+    })
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('matches')
+  }
+};
