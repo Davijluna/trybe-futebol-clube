5cb12d1a31a17380be63a85ff7e99a53f7c3429b
davi <davijluna@gmail.com>
2022-10-10 16:16:38 -0300

    feat: criando implementacao para requisito 15 e 16

---
+++ app/backend/src/controller/teamsController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import TeamsService from '../services/serviceTeam'
+
+export default class TeamsController {
+    static creat = async (req:Request, res:Response) => {
+        const response = await TeamsService.finAllService();
+        return res.status(response.code).json(response.message);
+    }
+
+    static getId = async (req: Request, res: Response) => {
+        const { id }  = req.params;
+        const result = await TeamsService.findByPkService(id);
+        return res.status(result.code).json(result.message);
+    }
+}
+// public teamService;
+// constructor() {
+//     this.teamService = new TeamsService();
+// }
+// public getAllController = async (req: Request, res: Response) => {
+//     const response = await this.teamService.findAll()
+// }
---
+++ app/backend/src/services/serviceTeam.ts
@@ -0,0 +1,14 @@
+import Team from '../database/models/Team';
+
+
+export default class TeamsService {
+    static async finAllService() {
+        const allTeams = await Team.findAll();
+        return { message: allTeams, code: 200 };
+    }
+
+    static async findByPkService (id: string) {
+        const teamGetId = await Team.findByPk(id);
+        return{ message: teamGetId, code: 200 };
+    };
+}
