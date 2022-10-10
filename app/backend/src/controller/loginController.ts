593e9f1af950b0577b0c6723fef62a1665a0e9aa
davi <davijluna@gmail.com>
2022-10-10 10:46:19 -0300

    feat: implemantando controller

--- app/backend/src/controller/loginController.ts
+++ app/backend/src/controller/loginController.ts
@@ -9,4 +9,22 @@ export default class LoginController {
     }
     return res.status(200).json({ token: response.data });
   }
+
+  static async chektoken(req: Request, res: Response ) {
+    const response = await UserServices.tokenValidation(req.headers.authorization);
+    if (response.erro) {
+      return res.status(response.code).json(response.erro);
+    }
+    return res.status(200).json({ role: response.role })
+  }
+
+  // static async getId(req: Request, res: Response) {
+  //   const result = await UserServices.veryficEmail(Number(req.params));
+  //   return res.status(200).json(result);
+  // }
+
+  // static async functionEmail(req: Request, res: Response) {
+  //   const response = await UserServices.veryficEmail(req.body);
+  //   return res.status(response.code).json(response.data);
+  // }
 }
