b614ce12e121972122af2d1af4126c9d642e3866
davi <davijluna@gmail.com>
2022-10-10 10:41:07 -0300

    feat: adicionando entities interface e middleware para login

---
+++ app/backend/src/middleware/middlewareLogin.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+
+const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/i;
+const Password = 6
+
+const validEmail = {
+
+    checkPassworEmail: (req: Request, res: Response, next: NextFunction) => {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: 'All fields must be filled' })
+        }
+        next();
+    },
+
+    checkEmail: (req: Request, res: Response, next: NextFunction) => {
+        const { email } = req.body;
+
+        if (!regexEmail.test(email)) {
+            return res.status(401).json({ message: 'Incorrect email or password' })
+        }
+
+        next();
+    },
+
+    checkPassword: (req: Request, res: Response, next: NextFunction) => {
+        const { password } = req.body;
+        if (password.length < Password) {
+            return res.status(401).json({ message: 'Password length must be more than 6 characters long' })
+        }
+
+        next();
+    },
+};
+
+export default validEmail
