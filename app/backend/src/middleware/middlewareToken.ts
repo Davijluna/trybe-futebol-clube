57f3a64fc5111ede0fd84251616a3b9cacd70a6d
davi <davijluna@gmail.com>
2022-10-10 10:48:03 -0300

    feat: criando middleware para token

--- app/backend/src/middleware/middlewareToken.ts
+++ app/backend/src/middleware/middlewareToken.ts
@@ -1,17 +1,22 @@
 import * as jwt from 'jsonwebtoken';
 import 'dotenv/config';
 
-const senha = process.env.JWT_SECRET || 'password';
+const secret = process.env.JWT_SECRET || 'jwt_secret';
 const jwtConfig: jwt.SignOptions = { expiresIn: '7d', algorithm: 'HS256' };
 
 const createToken = (email: any) => {
-  const token = jwt.sign({ email }, senha, jwtConfig);
+  const token = jwt.sign({ email }, secret, jwtConfig);
   return token;
 };
 
-// const checkToken = (token: any) => {
-//     const tokenCheck = jwt.verify(token, senha ) as jwt.JwtPayload;
-//     return tokenCheck
-// };
+const checkToken = (token: any) => {
+  try {
+    const tokenCheck = jwt.verify(token, secret ) as jwt.JwtPayload;
+    return tokenCheck
 
-export default createToken;
+  } catch (erro) {
+    return erro;
+  }
+};
+
+export { createToken, checkToken };
