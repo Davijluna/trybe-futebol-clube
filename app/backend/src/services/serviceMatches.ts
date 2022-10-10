705214c27cf294fe9f85bf9f034d4695c5cc37a8
davi <davijluna@gmail.com>
2022-10-10 17:42:55 -0300

    retactor: comentando arquivo pra ver teste no avalidador do github

--- app/backend/src/services/serviceMatches.ts
+++
@@ -1,8 +0,0 @@
-import Matches from '../database/models/Matches';
-
-export default class MatchesService {
-    static async finAllMatches() {
-    const result = await Matches.findAll();
-    return result;
-    };
-}
