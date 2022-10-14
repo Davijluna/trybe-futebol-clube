520202dc85a5ad75d4eca0f62dc3768734020a64
davi <davijluna@gmail.com>
2022-10-14 00:54:04 -0300

    feat: criando controller para requisito 25

--- app/backend/src/controller/matches.Controller.ts
+++ app/backend/src/controller/matches.Controller.ts
@@ -6,28 +6,17 @@ export default class MatchesController {
     const result = await MatchesService.finAllMatches();
     return res.status(200).json(result);
   };
-
+// requisito 24.
   static patchMatchController = async (req: Request, res: Response) => {
     const { id } = req.params;
      const result = await MatchesService.patchMatch(id)
     return res.status(200).json(result.message)
   }
-
+// requisito 23.
   static seveController = async (req: Request, res: Response) => {
     const requestBody =req.body;
     const { code, data } = await MatchesService.saveMatch(requestBody)
     return res.status(code).json(data)
   }
-
-  // static create = async (req: Request, res: Response) => {
-  //   const { id } = req.params;
-  //   const response = await MatchesService.createMatch(id);
-  //   return res.status(200).json(console.log(response));
-  // };
-  // static create = async(req: Request, res: Response) => {
-  //   const result = req.body
-  //   const response = await MatchesService.MatchUp(result);
-  //   return res.status(200).json(response);
-  // }
 
 }
