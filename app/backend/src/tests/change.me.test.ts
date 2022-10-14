5b1d6d2ac306399d441f125cd1e3fc6766d79ed6
davi <davijluna@gmail.com>
2022-10-14 19:10:33 -0300

    fix: correcao de testes de 80% do requisito 17

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -7,20 +7,31 @@ import { app } from '../app';
 // import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
+import UserModel from '../database/models/User';
 
 chai.use(chaiHttp);  //
 
 const { expect } = chai;
  //   RENOMEAR O NOME DO ARQUIVO OBS.
 describe('/login', () => {
+  const fakeUser =  {
+    username: 'User',
+    role: 'user',
+    email: 'user@user.com',
+    password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', 
+      // senha: secret_user
+  }
+
   describe('Primeiro caso de teste', () => {
     it('Deve cadastrar', async () => {
+      sinon.stub(UserModel, 'findOne').resolves(fakeUser as UserModel)
       const response = await chai.request(app).post('/login').send({
         email: 'user@user.com',  //
-        password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO',//
+        password: 'secret_user',//
       });
       expect(response.status).to.equal(200);
       expect(response.body).to.have.property('token')
+      sinon.restore()
     })
 
 // describe('/login/validate', () => {
