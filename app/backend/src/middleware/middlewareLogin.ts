544b021a3e53540508a7611d1b546482264cbff2
davi <davijluna@gmail.com>
2022-10-14 09:17:33 -0300

    feat:requidito 26  desenvolvendo middleware para verificar se não é possivel inserir um jogo em um time não existente

--- app/backend/src/middleware/middlewareLogin.ts
+++ app/backend/src/middleware/middlewareLogin.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import Team from '../database/models/Team';
 
 const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/i;
 const Password = 6;
@@ -34,12 +35,14 @@ const validEmail = {
     next();
   },
 
-  checkTeams: (req: Request, res: Response, next: NextFunction) => {
+  checkTeams: async (req: Request, res: Response, next: NextFunction) => {
     const { homeTeam, awayTeam } = req.body;
     if (homeTeam === awayTeam) {
       return res.status(401).json({ message: 'It is not possible to create a match with two equal teams' })
     }
-    if(!homeTeam && !awayTeam) {
+    const home = await Team.findOne({ where: { id: homeTeam, } });
+    const away = await Team.findOne({ where: { id: awayTeam, } });
+    if(!home || !away) {
       return res.status(404).json({ message: 'There is no team with such id!' })
     }
     next();
